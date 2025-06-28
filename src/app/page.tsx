6b7f2b9b904ecc4f0b22489b3a218ddda9ba40de
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-06-29 02:48:15 +0530

    middle part complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/Components/HeroSection";
+import FeaturedCourses from "@/Components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
