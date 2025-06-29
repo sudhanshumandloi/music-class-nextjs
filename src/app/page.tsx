4af0c20ee8572113bda25363a9625f75ac7b69da
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-06-29 14:31:38 +0530

    we complere the gradient part of the ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/Components/HeroSection";
 import FeaturedCourses from "@/Components/FeaturedCourses";
+import WhyChooseUs from "@/Components/WhyChooseUs";
+import TestimonialCards from "@/Components/TestimonialCards";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
     </main>
   );
 }
